138b09262650bfc486d0f77a228131dd649b7e9b
tatsu <tatsu100311@gmail.com>
2018-11-22 07:29:43 +0900

    Add theta optimize function

--- ex3/oneVsAll.m
+++ ex3/oneVsAll.m
@@ -49,16 +49,16 @@ X = [ones(m, 1) X];
 %                 initial_theta, options);
 %
 
+% Set Initial theta
+initial_theta = zeros(n + 1, 1);
+
+% Set options for fmincg
+options = optimset('GradObj', 'on', 'MaxIter', 50);
 
 
-
-
-
-
-
-
-
-
+for i = 1:num_labels
+    [all_theta(i,:)] = fmincg(@(t) (lrCostFunction(t, X, (y == i), lambda)), initial_theta, options);
+endfor
 
 % =========================================================================
 
